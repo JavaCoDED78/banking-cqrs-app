f9781eaf7165f01953868b848671becd2bd1371c
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-11 20:47:53 +0300

    init commit

---
+++ src/test/java/com/javade/BankingCqrsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCqrsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
