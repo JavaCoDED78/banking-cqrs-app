44803b0420790a29e7f12193fbcd0e76d35adac1
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-12 11:29:03 +0300

    Implement domain layer

---
+++ src/main/java/com/javade/domain/aggregate/Aggregate.java
@@ -0,0 +1,15 @@
+package com.javade.domain.aggregate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Aggregate {
+
+    private UUID id;
+}
