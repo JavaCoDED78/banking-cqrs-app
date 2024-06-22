e92c319403e093c27b5679b0dc3b6c1999b1b873
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-22 10:49:02 +0300

    #21 Add Transactional manager

--- src/main/java/com/javaded/service/account/AccountCommandServiceImpl.java
+++ src/main/java/com/javaded/service/account/AccountCommandServiceImpl.java
@@ -5,9 +5,11 @@ import com.javaded.events.AccountCreateEvent;
 import com.javaded.service.event.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AccountCommandServiceImpl implements AccountCommandService {
 
     private final EventService eventService;
