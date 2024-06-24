13774e44c220079e8cc62c31b223a29169eeca9b
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-24 12:56:34 +0300

    #25 Move common code to module

--- core-service/src/main/java/com/javaded/BankingCqrsAppApplication.java
+++ core-service/src/main/java/com/javaded/BankingCqrsAppApplication.java
@@ -2,12 +2,18 @@ package com.javaded;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableTransactionManagement
+@EntityScan(basePackages = "com.javaded")
+@ComponentScan(basePackages = "com.javaded")
+@EnableJpaRepositories(basePackages = "com.javaded.*")
 public class BankingCqrsAppApplication {
 
     public static void main(final String[] args) {
